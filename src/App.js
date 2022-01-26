c9fca301e4a8f0f5150ad98266066a0e8441daaa
nicky456 <sznici@gmail.com>
2022-01-26 14:21:02 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Home from "./Components/Home";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import CountryDetails from "./Components/CountryDetails";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
@@ -19,7 +20,7 @@ function App() {
   };
 
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
         <nav className={isDarkMode ? "nav" : "light nav"}>
           <Link to="/">
@@ -41,7 +42,7 @@ function App() {
           </Switch>
         </div>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
